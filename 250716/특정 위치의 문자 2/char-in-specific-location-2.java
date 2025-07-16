8679857f121817145e74b261f437f3ba92cd7196
estiemoon <102018454+SeungeunMoon@users.noreply.github.com>
2025-07-16 09:13:41 +0900

    Title: 특정 위치의 문자 2, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250716/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] alpha = sc.nextLine().split(" ");
+
+        
+        System.out.print(alpha[1]+" ");
+        System.out.print(alpha[4]+" ");
+        System.out.print(alpha[7]);
+    }
+}
