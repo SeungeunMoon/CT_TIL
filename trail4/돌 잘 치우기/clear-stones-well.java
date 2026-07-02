261da154846b049ea608780f42ac6dcb336c1ad3
estiemoon <102018454+SeungeunMoon@users.noreply.github.com>
2026-07-02 16:15:40 +0900

    [Passed] 돌 잘 치우기 (93ms, 9MB)

--- trail4/돌 잘 치우기/clear-stones-well.java
+++ trail4/돌 잘 치우기/clear-stones-well.java
@@ -1,117 +1,117 @@
 import java.util.*;
 import java.io.*;
+class Pair{
+    int x,y;
+    public Pair(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
 
 public class Main {
-    static int n,m,k,maxCnt;
-    static int[][] graph, curGraph;
-    static boolean[][] visited;
-    static int[] dr = {-1,1,0,0}, dc = {0,0,-1,1};
-    static ArrayList<Point> rocks;
-    static Point[] starts;
-    static int[] picked;
+    private static List<Pair> start =new ArrayList<>();
+    private static List<Pair> stone=new ArrayList<>();
+    private static List<Pair> select=new ArrayList<>();
+    private static int n,k,m,result=0;
+    private static boolean[][] visited;
+    private static int[][] grid;
 
-    static class Point {
-        int r,c;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        m =Integer.parseInt(st.nextToken());
 
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
+        visited=new boolean[n][n];
+
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st=new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+                if(grid[i][j]==1){
+                    stone.add(new Pair(i,j));
+                }
+            }
         }
+     
+        for (int i = 0; i < k; i++) {
+            st=new StringTokenizer(br.readLine());
+            int r=Integer.parseInt(st.nextToken())-1;
+            int c=Integer.parseInt(st.nextToken())-1;
+            start.add(new Pair(r,c));
+        }
+        per(0,0);
+        System.out.println(result);
+        
     }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken()); 
-        k = Integer.parseInt(st.nextToken()); 
-        m = Integer.parseInt(st.nextToken()); 
-
-        graph = new int[n][n];
-        curGraph = new int[n][n];
-        rocks = new ArrayList();
-        starts = new Point[k];
-        picked = new int[m];
-
-        for(int i=0; i<n;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                if(graph[i][j] == 1) {
-                    rocks.add(new Point(i,j));
-                }
-            }
-        }
-
-        for(int i=0; i< k; i++) {
-            st = new StringTokenizer(br.readLine());
-            starts[i] = new Point(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1);
-        }
- 
-        combi(0,0);
-
-        System.out.print(maxCnt);
-
-    }
-
-    static void combi(int cur, int depth) {
-
-        if(depth == m) {
-            visited = new boolean[n][n];
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n;j++) {
-                    curGraph[i][j] = graph[i][j];
-                }
-            }
-
-            for(int i=0; i<picked.length; i++) {
-                Point p = rocks.get(picked[i]);
-                curGraph[p.r][p.c] = 0;
-            }
-            BFS();
-            int cnt = 0;
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n;j++) {
-                    if(visited[i][j]) {
-                        cnt++;
-                    }
-                }
-            }
-            maxCnt = Integer.max(cnt,maxCnt);
+    // 돌 치울 자리 
+    private static void per(int index,int cnt){
+       
+        if(cnt==m){
+            result= Math.max(result,calc());
             return;
         }
-
-        for(int i=cur; i<rocks.size(); i++) {
-            picked[depth] = i;
-            combi(i+1, depth+1);
+        if(index==stone.size()){
+            return;
         }
+        select.add(stone.get(index));
+        per(index+1,cnt+1);
+        select.remove(select.size()-1);
+        per(index+1,cnt);
+        
+    }
+    private static int calc(){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                visited[i][j]=false;
+            }
+        }
+        //돌 임시 치우기
+        for(int i=0;i<m;i++){
+            Pair pos= select.get(i);
+            grid[pos.x][pos.y]=0;
+        }
+        bfs();
+        //돌 복구
+        for(int i=0;i<m;i++){
+            Pair pos= select.get(i);
+            grid[pos.x][pos.y]=1;
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(visited[i][j]) cnt++;
+            }
+        }
+        return cnt;
 
     }
+    private static void bfs(){
+        int[] dx={1,-1,0,0};
+        int[] dy={0,0,-1,1};
 
-    static void BFS() {
-        Queue<Point> q = new ArrayDeque();
-
-        for(Point p: starts) {
-            q.add(p);
-            visited[p.r][p.c] = true;
+        Queue<Pair> q=new LinkedList<>();
+    
+        for(int i=0;i<k;i++){
+            q.add(start.get(i));
+            visited[start.get(i).x][start.get(i).y]=true;
         }
 
-        while(!q.isEmpty()) {
+        while(!q.isEmpty()){
+            Pair curr= q.poll();
+            int x=curr.x;
+            int y=curr.y;
+            for(int i=0;i<4;i++){
+                int nx=curr.x+dx[i];
+                int ny=curr.y+dy[i];
 
-            Point cur = q.poll();
-
-            for(int i=0; i<4; i++) {
-                int nr = cur.r + dr[i];
-                int nc = cur.c + dc[i];
-
-                if(nr>=0 && nc>=0 && nr<n && nc<n) {
-                    if(!visited[nr][nc] && curGraph[nr][nc]==0) {
-                        visited[nr][nc] =true;
-                        q.add(new Point(nr,nc));
-                    }
+                if(nx>=0 &&nx<n &&ny>=0 &&ny<n &&!visited[nx][ny]&&grid[nx][ny]==0){
+                    q.add(new Pair(nx,ny));
+                    visited[nx][ny]=true;
                 }
             }
-
         }
-
     }
 }
