1b6fba6d78e348d449b6c429c54342230966cb71
estiemoon <102018454+SeungeunMoon@users.noreply.github.com>
2026-07-27 17:20:18 +0900

    [Passed] treemap 기본 (283ms, 18MB)

--- trail5/treemap 기본/treemap-basic.java
+++ trail5/treemap 기본/treemap-basic.java
@@ -1,54 +1,59 @@
 import java.util.*;
-import java.util.Map.Entry;
+import java.util.Map.*;
 import java.io.*;
 
 public class Main {
+    private static TreeMap<Integer,Integer> map=new TreeMap<>();
     public static void main(String[] args) throws IOException{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb=new StringBuilder();
 
-        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        int n=Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
+        while(n>0){
+            st=new StringTokenizer(br.readLine());
+            String type= st.nextToken();
 
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
 
-            switch(cmd) {
-                case "add":
-                    int a = Integer.parseInt(st.nextToken());
-                    int b = Integer.parseInt(st.nextToken());
-                    tm.put(a,b);
-                    break;
-                case "find":
-                    int f = Integer.parseInt(st.nextToken());
-                    if(tm.getOrDefault(f, -1) != -1) sb.append(tm.get(f)).append("\n");
-                    else sb.append("None").append("\n");
-                    break;
-                case "remove":
-                    int r = Integer.parseInt(st.nextToken());
-                    tm.remove(r);
-                    break;
-                case "print_list":
-                    Iterator<Entry<Integer,Integer>> it = tm.entrySet().iterator();
+            if(type.equals("add")){
+                int k=Integer.parseInt(st.nextToken());
+                int v=Integer.parseInt(st.nextToken());
+                map.put(k,v);
 
-                    if(it.hasNext() == false) {
-                        sb.append("None").append("\n"); break;
+            }else if(type.equals("find")){
+                int k=Integer.parseInt(st.nextToken());
+                int num= map.getOrDefault(k,-1);
+                if(num!=-1){
+                    sb.append(num);
+                }else{
+                    sb.append("None");
+                }
+                sb.append('\n');
+
+            }else if(type.equals("remove")){
+                int k=Integer.parseInt(st.nextToken());
+                map.remove(k);
+
+            }else if(type.equals("print_list")){
+                Iterator<Entry<Integer,Integer>> it=map.entrySet().iterator();
+
+                if(!it.hasNext()){
+                    sb.append("None");
+                }else{
+                    while(it.hasNext()){
+                        Entry<Integer,Integer> entry=it.next();
+                        sb.append(entry.getValue()).append(' ');
                     }
-                    while(it.hasNext()) {
-                        Entry<Integer, Integer> entry = it.next();
-                        sb.append(entry.getValue()).append(" ");
-                    }
-                    sb.append("\n");
-                    break;
+                }
+                sb.append('\n');
             }
+
+            n--;
         }
 
-        System.out.print(sb);
 
+        System.out.print(sb);
 
     }
 }
