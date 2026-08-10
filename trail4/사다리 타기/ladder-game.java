b7233a45810b645cd386db178099d5e9ec010766
estiemoon <102018454+SeungeunMoon@users.noreply.github.com>
2026-08-10 17:29:31 +0900

    [Passed] 사다리 타기 (98ms, 9MB)

--- trail4/사다리 타기/ladder-game.java
+++ trail4/사다리 타기/ladder-game.java
@@ -1,76 +1,76 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    private static int n,m;
-    static class Line{
+    static class Edge{
         int a,b;
-        public Line(int a,int b){
+        public Edge(int a,int b){
             this.a=a;
             this.b=b;
         }
     }
-    private static List<Line> lines=new ArrayList<>();
-    private static List<Line> selected =new ArrayList<>();
+    private static int n,m,ans=Integer.MAX_VALUE;
+    private static List<Edge> list=new ArrayList<>();
+    private static List<Edge> selected=new ArrayList<>();
     private static int[] origin;
-    private static int ans=Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException{
-        // Please write your code here.
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        
         n=Integer.parseInt(st.nextToken());
         m=Integer.parseInt(st.nextToken());
+
+        origin=new int[n+1];
 
         for(int i=0;i<m;i++){
             st=new StringTokenizer(br.readLine());
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
-            lines.add(new Line(a,b));
+            list.add(new Edge(a,b));
         }
 
-        origin=runLadder(lines);
-        backtracking(0);
+        origin=calculate(list);
+        dfs(0);
         System.out.println(ans);
+
+        
     }
-    private static void backtracking(int idx){
+    private static void dfs(int idx){
         if(idx==m){
-            int[] curr=runLadder(selected);
+            int[] curr= calculate(selected);
             if(Arrays.equals(origin,curr)){
                 ans=Math.min(ans,selected.size());
             }
             return;
         }
-
-        //가로줄 선택
-        selected.add(lines.get(idx));
-        backtracking(idx+1);
+        //선택
+        selected.add(list.get(idx));
+        dfs(idx+1);
         selected.remove(selected.size()-1);
-
-        //2. 선택 안함
-        backtracking(idx+1);
+        //선택X
+        dfs(idx+1);
     }
-    private static int[] runLadder(List<Line> lineList){
+    private static int[] calculate(List<Edge> curr){
         int[] result=new int[n+1];
 
-        for(int i=1;i<=n;i++){
-            int currNum=i;
+        for(int i=1;i<n+1;i++){
+            int index=i;
 
-            //(높이 b) 순서대로 가로줄 처리
-            //높이 b가 1부터 15까지 
             for(int height=1;height<=15;height++){
-                for(Line line : lineList){
+                for(Edge line: curr){
                     if(line.b==height){
-                        if(line.a==currNum){
-                            currNum++;
-                        }else if(line.a+1==currNum){
-                            currNum--;
+                        if(line.a==index){
+                            index++;
+                        }else if(line.a==index-1){
+                            index--;
                         }
                     }
                 }
             }
-            result[i]=currNum;
+            result[i]=index;
         }
         return result;
-
     }
 }
